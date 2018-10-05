607f3e13cce775cd62e07601c173267c1e0fb47a
Jennifer <you@example.com>
2018-10-05 11:59:00 +0200

    Jusqu'à question 11

---
+++ src/main/java/com/ipiecoles/java/java210/Main.java
@@ -0,0 +1,12 @@
+package com.ipiecoles.java.java210;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println(Sudoku.FIN_SAISIE);
+
+        Sudoku s = new Sudoku();
+
+        s.demandeCoordonneesSudoku();
+    }
+}
--- src/main/java/com/ipiecoles/java/java210/Sudoku.java
+++ src/main/java/com/ipiecoles/java/java210/Sudoku.java
@@ -1,17 +1,55 @@
 package com.ipiecoles.java.java210;
 
+import java.util.Scanner;
+
 public class Sudoku {
+		public static final String FIN_SAISIE = "FIN";
+
+		public boolean resolu = false;
+
+		public short [][] sudokuAResoudre;
+
+		public short [][] getSudokuAResoudre ()
+		{
+			return sudokuAResoudre;
+		}
+
+		public void setSudokuAResoudre (short [][] tab)
+		{
+			sudokuAResoudre = tab;
+		}
 
 	/**
 	 * Constructeur par défaut
 	 */
 	public Sudoku() {
+		sudokuAResoudre = new short [9] [9];
+		//autre méthode : setSudokuAResoudre(new short [9][9])
 	}
 
 	public static boolean ligneSaisieEstCoherente(String ligneSaisie) {
+		//ligne ne doit pas être nulle ou vide
+		if (ligneSaisie == null || ligneSaisie.trim().equals(""))
+		{
+			System.out.println("Les coordonnées du chiffre et/ou sa valeur ne peuvent pas être nulles, vides ou remplies avec des espaces");
+			return false;
+		}
+		//ligne doit faire 3 caractères
+		else if(ligneSaisie.length() != 3)
+		{
+			System.out.println("Les coordonnées du chiffre et/ou sa valeur doit faire 3 caractères");
+			return false;
+		}
+		//1er chiffre doit être entre 0 et 8, 2ème doit être entre 0 et 8 et le 3ème entre 1 et 9
+		//utiliser un '!' devant ligneSaisie pour être sûr de bien avoir false si c'est false et non l'inverse
+		else if(!ligneSaisie.matches("[0-8][0-8][1-9]"))
+		{
+			System.out.println("L'abscisse et l'ordonnée doivent être compris entre 0 et 8, la valeur entre 1 et 9");
+			return false;
+		}
 		return true;
 	}
-	
+
 	/**
 	 * Cette méthode invite l'utilisateur à saisir un ensemble de coordonnées pour initialiser un sudoku à résoudre.
 	 * Les coordonnées prennent la forme XYZ avec X correspondant à l'abscisse, Y l'ordonnée et Z la valeur. Seules les
@@ -26,7 +64,36 @@ public class Sudoku {
 	 * @return Un tableau comportant les coordonnées des chiffres présents dans le sudoku à résoudre
 	 */
 	public static String[] demandeCoordonneesSudoku() {
-		return null;
+		//Un sudoku fait 9 par 9, soit potentiellement 81 valeurs à remplir même si ce n'est pas très intéressant
+		String[] tableauCoordonnees = new String[81]; //on déclare le tableau de coordonnes
+
+		Scanner scanner = new Scanner(System.in); //permet de récupérer la saisie de l'utilisateur
+
+		String ligneSaisie = scanner.nextLine(); // bloque tout tant que l'utilisateur n'a rien saisi ni tapé Entrée
+
+		int i =0;
+
+		while (!ligneSaisie.equals("FIN"))
+			//Tester si ligne cohérente
+			//Si oui, on la rentre dans le tableau
+			//Si non, on informe l'utilisateur de l'erreur et lui redemande de saisir de nouvelles coordonnées
+		{
+			if (ligneSaisieEstCoherente(ligneSaisie))
+			{
+				tableauCoordonnees[i] = ligneSaisie; //la saisie de l'utilisateur sera aux coordonnées 'i'
+				i++; //on passe à la coordonnée suivante
+			}
+			else
+			{
+				System.out.println("Coordonnées fausses, réessayer");
+			}
+
+			ligneSaisie = scanner.nextLine(); //Pour passer à la ligne de saisie suivante
+		}
+
+		scanner.close(); //Plus de saisie à faire, on ferme donc le scanner.
+
+		return tableauCoordonnees; //on renvoie le tableau
 	}
 	
 	/**
